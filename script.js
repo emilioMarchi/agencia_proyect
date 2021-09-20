b0962627f48ae94ca245cc1de46bb3eb1e8d3042
emilioMarchi <em.98.nmynm@gmail.com>
2021-09-20 15:09:20 -0300

    add function cards-services and card-planes

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
+$(document).ready( () => {
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+    
+    $('.cards-planes').on('click', (e) => {
+        const element = e.target;
+        const id = e.target.id;
+        const addDescription = (i) => {
+            $(`#${id}>p`).toggle(500);
+        }
+        if(id.length >= 1) {
+            addDescription(id)
+        } else {
+            const card = $(element).closest('.cards-planes');
+            const cardId = card.attr('id');
+            $(`#${cardId}>p`).toggle(500);
+        }
+    })
+    $('.cards-services').on('click', (e) => {
+        const element = e.target;
+        const id = e.target.id;
+        const addDescription = (i) => {
+            $(`#${id}>p`).toggle(500);
+        }
+        if(id.length >= 1) {
+            addDescription(id)
+        } else {
+            const card = $(element).closest('.cards-services');
+            const cardId = card.attr('id');
+            $(`#${cardId}>p`).toggle(500);
+        }
+    })
+
+})
